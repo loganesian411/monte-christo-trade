b31ca6e43e8b602de0dda0e7030cb4895e7094ac
loganesian411 <loganesian411@gmail.com>
2017-03-05 22:04:03 -0800

    adding the rest of dashboard boiler plate files.

---
+++ dashboard/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url                                                
+                                                                                                                                                           
+from . import views                                                                                                                                        
+                                                                                                                                                           
+app_name = 'dashboard'
+urlpatterns = []
+#urlpatterns = [url(r'^$', views.order_form, name='order_form'),]
