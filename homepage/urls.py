3e106db3a27be3616e21e0ec947f379993eae879
loganesian411 <loganesian411@gmail.com>
2017-04-17 23:09:16 -0700

    updated views to include the new contact form and created a temporary success redirect

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'homepage'
 urlpatterns = [url(r'^$', views.index, name='index'),
+               url(r'^success$', views.message_success,
+                   name='message-success'),
                url(r'^machines/(?P<machine_name>\w+)/$',
                    views.machine_detail,
                    name='machine-detail'),]
--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,27 @@
+from django.core.mail import send_mail
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+import forms
 
 # Create your views here.
 def index(request):
-  return render(request, 'homepage/index.html')
+  if request.method == 'POST':
+    contact_form = forms.ContactForm(request.POST)
+    if contact_form.is_valid():
+      name = contact_form.cleaned_data['name']
+      subject = contact_form.cleaned_data['subject']
+      sender_email = contact_form.cleaned_data['sender_email']
+      message = contact_form.cleaned_data['message']
+      send_mail(subject, message, sender_email, ['test@example.com'])
+      return HttpResponseRedirect(reverse('homepage:message-success'))
+  else:
+	  contact_form = forms.ContactForm()
+  return render(request, 'homepage/index.html', {'form': contact_form})
+
+def message_success(request):
+  return HttpResponse(('Thank you for contacting us! We will get back to you as soon as possible'))
 
 def machine_detail(request, machine_name):
   template = 'homepage/{0}.html'.format(machine_name)
