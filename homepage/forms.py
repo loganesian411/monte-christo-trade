0831c410a071fa522fd49744f8751f69814028bc
loganesian411 <loganesian411@gmail.com>
2017-04-20 23:07:57 -0700

    fixed the styling for the contact form

--- homepage/forms.py
+++ homepage/forms.py
@@ -5,4 +5,10 @@ class ContactForm(forms.Form):
   subject = forms.CharField(max_length=100)
   message = forms.CharField(widget=forms.Textarea)
   sender_email = forms.EmailField()
+
+  def __init__(self, *args, **kwargs):
+    super(ContactForm, self).__init__(*args, **kwargs)
+    for field in iter(self.fields):
+      self.fields[field].widget.attrs.update({'class': 'form-control'})
+    self.fields['message'].widget.attrs.update({'rows': '15'})
   
