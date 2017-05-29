a1324e55e7da60314576703814339a4ec30d4075
loganesian411 <loganesian411@gmail.com>
2017-05-29 13:44:30 -0700

    Removing django files.

--- webstore/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-app_name = 'webstore'
-urlpatterns = [url(r'^$', views.purchase_order.as_view(), name='purchase-order'),
-               url(r'^confirmation/$', views.confirmation, name='confirmation'),]
